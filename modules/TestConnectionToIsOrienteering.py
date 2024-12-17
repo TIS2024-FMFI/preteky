6ba7c26fd91aa88e1c95613a6f03d5bcbdfabf76
cheetahzlesa <kozuch8@uniba.sk>
2024-12-17 16:48:23 +0100

    added another test case

--- modules/TestConnectionToIsOrienteering.py
+++ modules/TestConnectionToIsOrienteering.py
@@ -2,7 +2,6 @@ import utilities.ErrorHandler as e
 from GetFromIsOrienteering import Mod_get
 import PostToIsOrienteering as post
 import unittest
-import json
 
 API_ENDPOINT = 'https://is.orienteering.sk/api'
 API_KEY = 'NjUxODU1ZDE3YTEyMA=='
@@ -11,7 +10,8 @@ WRONG_API_KEY = 'NjUxODU1ZDE3YTEyMA'
 RACE_ID = None
 CLUB_ID = 81
 RUNNER_ID = None
-
+CATEGORY_ONE = None
+CATEGORY_TWO = None
 
 class ModGetTest(unittest.TestCase):
     def test_bad_endpoint_get(self):
@@ -41,7 +41,7 @@ class ModGetTest(unittest.TestCase):
             self.assertEqual(er.code, 404)
             self.assertEqual(er.message, 'Not Found https://is.orienteering.sk/lists/category')
 
-    def test_get_races_in_month(self):
+    def test_get_info(self):
         g = Mod_get(API_ENDPOINT, API_KEY)
         races = g.get_races_in_month("November")
         self.assertIsNotNone(races)
@@ -49,21 +49,26 @@ class ModGetTest(unittest.TestCase):
         self.assertIsNotNone(RACE_ID)
         ### doplnit test ci preteky su v datume
 
-    def test_get_club_registrations(self):
-        g = Mod_get(API_ENDPOINT, API_KEY)
         club_registrations = g.get_club_registrations(CLUB_ID)
         self.assertIsNotNone(club_registrations)
         self.assertEqual(club_registrations[0]["runner"]["first_name"], "Richard")
         RUNNER_ID = club_registrations[0]["runner_id"]
         self.assertIsNotNone(RUNNER_ID)
-        
-    def test_get_runner(self):
-        g = Mod_get(API_ENDPOINT, API_KEY)
-        print(RUNNER_ID)
+
         runner = g.get_runner(RUNNER_ID)
-        print(runner)
         self.assertIsNotNone(runner)
         self.assertEqual(runner[0]["first_name"], "Richard")
+
+        details = g.get_race_details(RACE_ID)
+        self.assertIsNotNone(details)
+        self.assertEqual(len(details["categories"]), 6)
+        CATEGORY_ONE, CATEGORY_TWO = details["categories"][:2] 
+        self.assertIsNotNone(CATEGORY_ONE)   
+        self.assertIsNotNone(CATEGORY_TWO)
+        self.assertEqual(CATEGORY_ONE["category_id"], '160')
+        self.assertEqual(CATEGORY_TWO["category_id"], '161')
+
+        
 
 if __name__ == '__main__':
     unittest.main()
